f5d1fa30a78eaa650c484ea1ab6c88c219ef2f15
coderrob22 <robertknott7@gmail.com>
2025-12-08 14:17:15 -0500

    friend form with button

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App(){
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
       </div>
     </div>)
 }
@@ -60,6 +61,25 @@ function Friend ({friend}) {
           You and {friend.name} are even. 
         </p>
       )}
-      <button className="button">Select</button>
-    </li>)
+      <Button>Select</Button>
+    </li>
+    );
+}
+
+function Button ( {children} ){
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend(){
+  return (
+    <form className="form-add-friend">
+      <label>Friend name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  )
 }
